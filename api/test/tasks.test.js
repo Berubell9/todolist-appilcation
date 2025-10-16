81fc460f50a1d3718c9b2cfaebe0b5905473454e
Klinglunlun <79560837+Berubell9@users.noreply.github.com>
2025-10-16 13:55:28 +0700

    Update tasks.test.js

--- api/test/tasks.test.js
+++ api/test/tasks.test.js
@@ -6,3 +6,7 @@ test("GET /tasks should respond 200", async () => {
   // อาจได้ 200 หรือ 500 ถ้า DB ยังไม่พร้อม
   expect([200, 500]).toContain(res.statusCode);
 });
+afterAll(async () => {
+  // ปิด MySQL pool เพื่อไม่ให้มี open handles
+  await pool.end();
+});
